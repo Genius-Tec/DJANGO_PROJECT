b37910295e8ce948de25c12c13bbe3946f52f596
Genius-Tec <binbikinlonteb@gmail.com>
2025-08-19 22:52:24 -0700

    Add files via upload

---
+++ store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/product_list.html', context)
